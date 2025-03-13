ee9a58ec2d5f7257d969f060c76bfc5afa0f8cc0
Yuji Uono <yuuono.1998@gmail.com>
2025-03-13 20:32:57 +0100

    Done

--- index.js
+++ index.js
@@ -1,17 +1,64 @@
 class SortedList {
-  constructor() {}
+  constructor()
+{
+  this.items = [];
+  this.length = 0;
 
-  add(item) {}
+}
 
-  get(pos) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort ((a, b) => a - b)
+    this.length++
+  }
 
-  max() {}
+  
 
-  min() {}
+  get(pos) {
+   /*  if ( pos < 0 || pos >= this.length) {
+      throw new Error("OutOfBounds");
+    }
+    return this.items[pos];
+  } */
 
-  sum() {}
+  if ( this.items[pos] ) {
+    return this.items[pos]
+  } else {
+    throw new Error("OutOfBounds");
+  }
+  }
 
-  avg() {}
+  max() {
+    if ( this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return Math.max(...this.items);
+    
+  }
+
+  min() {
+    if ( this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return Math.min(...this.items);
+    
+  }
+
+  sum() {
+    if (this.items.length === 0) {
+      return 0 ;
+  }
+  return this.items.reduce((a, b) => a + b, 0);
+  }
+  avg() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return this.sum() / this.items.length;
+  }
+
+
+
 }
 
 module.exports = SortedList;
